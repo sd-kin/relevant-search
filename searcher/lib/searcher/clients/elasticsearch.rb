c6ff603ee720e80d54f7a2d2ccaafb74c01c9403
Dmitry Sidorov <sd-kin@rambler.ru>
2018-12-09 13:20:10 +0300

    move clients under searcher namespace

--- searcher/lib/searcher/clients/elasticsearch.rb
+++ searcher/lib/searcher/clients/elasticsearch.rb
@@ -1,37 +1,38 @@
 require 'httparty'
 
-module Clients
-  # TODO put it in Searcher namespace
-  # http client for ElasticSearch
-  class ElasticSearch
-    include HTTParty
+module Searcher
+  module Clients
+    # http client for ElasticSearch
+    class ElasticSearch
+      include HTTParty
 
-    attr_reader :settings
+      attr_reader :settings
 
-    base_uri 'http://localhost:9200'
+      base_uri 'http://localhost:9200'
 
-    def initialize(analysis_settings: {}, mappings_settings: {})
-      @settings = { settings: { number_of_shards: 1, index: {} } }
-      settings.fetch(:settings).fetch(:index)[:analysis] = analysis_settings
-      settings[:mappings] = mappings_settings
-    end
+      def initialize(analysis_settings: {}, mappings_settings: {})
+        @settings = { settings: { number_of_shards: 1, index: {} } }
+        settings.fetch(:settings).fetch(:index)[:analysis] = analysis_settings
+        settings[:mappings] = mappings_settings
+      end
 
-    def create(index = '/tmdb')
-      self.class.put(index, settings)
-    end
+      def create(index = '/tmdb')
+        self.class.put(index, settings)
+      end
 
-    def destroy(index = '/tmdb')
-      self.class.delete(index)
-    end
+      def destroy(index = '/tmdb')
+        self.class.delete(index)
+      end
 
-    def index(data)
-      self.class.post('/_bulk', headers: { 'Content-Type' => 'application/json' }, body: data)
-    end
+      def index(data)
+        self.class.post('/_bulk', headers: { 'Content-Type' => 'application/json' }, body: data)
+      end
 
-    def reindex(data)
-      destroy
-      create
-      index(data)
+      def reindex(data)
+        destroy
+        create
+        index(data)
+      end
     end
   end
 end
--- searcher/spec/lib/searcher/clients/elasticsearch_spec.rb
+++ searcher/spec/lib/searcher/clients/elasticsearch_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 require_relative File.join(Searcher.root, 'lib', 'searcher', 'clients', 'elasticsearch.rb')
 
-describe Clients::ElasticSearch do
-  let(:client) { Clients::ElasticSearch.new }
+describe Searcher::Clients::ElasticSearch do
+  let(:client) { Searcher::Clients::ElasticSearch.new }
 
   describe '#new' do
     context 'when initialized without params' do
-      let(:settings) { Clients::ElasticSearch.new.settings }
+      let(:settings) { Searcher::Clients::ElasticSearch.new.settings }
 
       it 'uses config with one shard' do
         expect(settings.dig(:settings, :number_of_shards)).to eq 1
@@ -22,7 +22,7 @@ describe Clients::ElasticSearch do
     end
 
     context 'when initialized with params' do
-      let(:settings) { Clients::ElasticSearch.new(config).settings }
+      let(:settings) { Searcher::Clients::ElasticSearch.new(config).settings }
 
       context 'for mappings settings' do
         let(:config) { { analysis_settings: {}, mappings_settings: { test: 'test' } } }
@@ -47,14 +47,14 @@ describe Clients::ElasticSearch do
 
     context 'when no arguments given' do
       it 'sends put request for default index' do
-        expect(Clients::ElasticSearch).to receive(:put).with('/tmdb', default_config)
+        expect(Searcher::Clients::ElasticSearch).to receive(:put).with('/tmdb', default_config)
         client.create
       end
     end
 
     context 'when index name given' do
       it 'sends put request for given index' do
-        expect(Clients::ElasticSearch).to receive(:put).with('/test', default_config)
+        expect(Searcher::Clients::ElasticSearch).to receive(:put).with('/test', default_config)
         client.create('/test')
       end
     end
@@ -63,24 +63,24 @@ describe Clients::ElasticSearch do
   describe '#destroy' do
     context 'when no arguments given' do
       it 'sends delete request for default index' do
-        expect(Clients::ElasticSearch).to receive(:delete).with('/tmdb')
+        expect(Searcher::Clients::ElasticSearch).to receive(:delete).with('/tmdb')
         client.destroy
       end
     end
 
     context 'when index name given' do
       it 'sends delete request for given index' do
-        expect(Clients::ElasticSearch).to receive(:delete).with('/test')
+        expect(Searcher::Clients::ElasticSearch).to receive(:delete).with('/test')
         client.destroy('/test')
       end
     end
   end
 
   describe '#index' do
-    let(:client) { Clients::ElasticSearch.new }
+    let(:client) { Searcher::Clients::ElasticSearch.new }
 
     it 'sends post request for given index' do
-      expect(Clients::ElasticSearch).to receive(:post).with('/_bulk', headers: { 'Content-Type' => 'application/json' }, body: 'test')
+      expect(Searcher::Clients::ElasticSearch).to receive(:post).with('/_bulk', headers: { 'Content-Type' => 'application/json' }, body: 'test')
       client.index('test')
     end
   end
